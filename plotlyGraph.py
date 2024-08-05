685eec102d5e03cd48cc1f551dfb778da3c92f08
Christopher Chan <98066805+CChan101@users.noreply.github.com>
2024-08-04 23:42:01 -0400

    Add files via upload

--- plotlyGraph.py
+++ plotlyGraph.py
@@ -393,9 +393,9 @@ def update_conditional_text(given_tier):
     })
 
 
-# Run the app
 if __name__ == '__main__':
-    app.run_server(debug=False, host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))
+    app.run_server(debug=False, host='0.0.0.0', port=port)
 
 
 def update_graph_callback(top_n, given_tier, ladder_ranking):
